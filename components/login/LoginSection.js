7b68d8703cb4f5bd1986e893c052b9ef4b3415e5
강승현 <number42isuser@gmail.com>
2025-04-08 11:20:37 +0900

    ✨ feat: login, main 스크린 파일 추가

---
+++ components/login/LoginSection.js
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native'
+import React from 'react'
+
+const LoginSection = () => {
+  return (
+    <View>
+      <Text>LoginSection</Text>
+    </View>
+  )
+}
+
+export default LoginSection
