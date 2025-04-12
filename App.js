d017cc898bf9e11b6531af4b89bc760a9846593b
seunghyeonKang <number42isuser@gmail.com>
2025-04-12 22:24:41 +0900

    ✨ feat: 네비게이션 기능 추가

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import MainScreen from './screens/MainScreen';
-import LoginScreen from './screens/LoginScreen';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import LoginScreen from "./screens/LoginScreen";
+import MainScreen from "./screens/MainScreen";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <LoginScreen/>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Main" component={MainScreen} options={{ headerShown: false }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Platform, Text, View } from 'react-native'
+import React from "react";
+import { Platform, Text, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
@@ -14,7 +14,7 @@ const LoginScreen = () => {
         </View>
       </SafeAreaView>
     </SafeAreaProvider>
-  )
-}
+  );
+};
 
-export default LoginScreen
+export default LoginScreen;
