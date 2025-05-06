d38954a752f73447d75f36599ecf679fc3795093
KaHeyon <diddl2002@naver.com>
2025-05-06 22:41:54 +0900

    🎨 design: 선택완료 버튼 UI 수정

--- screens/CreateScreen.js
+++ screens/CreateScreen.js
@@ -1,7 +1,8 @@
-import { Button, Text, TouchableOpacity, View } from "react-native";
+import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React, { useState } from "react";
 import Layout from "../layouts/Layout";
-import Ionic from 'react-native-vector-icons/Ionicons';
+import CategoryCard from "../components/create/CategoryCard";
+import CustomHeader from "../components/CustomHeader";
 import { useNavigation } from "@react-navigation/native";
 
 
@@ -10,109 +11,106 @@ const CreateScreen = () => {
   const [selectedCategory, setSelectedCategory] = useState(null);
 
   const categories = [
-    { category: "cotent", id: "book", label: "책", width: 50 },
-    { category: "cotent", id: "movie", label: "영화/드라마", width: 105 },
-    { category: "cotent", id: "webtoon", label: "웹툰/웹소설/만화", width: 139 },
-    { category: "cotent", id: "music", label: "음악", width: 60 },
-    { category: "cotent", id: "audio", label: "팟캐스트/오디오", width: 131 },
-    { category: "cotent", id: "game", label: "게임", width: 60 },
-    { category: "cotent", id: "documentary", label: "강의/다큐멘터리", width: 131 },
-    { category: "cotent", id: "etc", label: "기타 콘텐츠", width: 102 },
+    { id: "book", label: "책" },
+    { id: "movie", label: "영화/드라마" },
+    { id: "webtoon", label: "웹툰/웹소설/만화" },
+    { id: "music", label: "음악" },
+    { id: "audio", label: "팟캐스트/오디오" },
+    { id: "game", label: "게임" },
+    { id: "documentary", label: "강의/다큐멘터리" },
+    { id: "etc content", label: "기타 콘텐츠" },
 
-    { category: "culture", id: "art", label: "전시/아트 페어", width: 121 },
-    { category: "culture", id: "musical", label: "연극/뮤지컬", width: 103 },
-    { category: "culture", id: "concert", label: "콘서트/음악 페스티벌", width: 163 },
-    { category: "culture", id: "sport", label: "스포츠 경기", width: 102 },
-    { category: "culture", id: "festival", label: "축제", width: 56 },
-    { category: "culture", id: "dance", label: "무용/발레", width: 89 },
-    { category: "culture", id: "classic", label: "클래식/국악 공연", width: 135 },
-    { category: "culture", id: "cultures", label: "체험형 문화활동", width: 130 },
-    { category: "culture", id: "etc culture", label: "기타 문화", width: 88 },
+    { id: "art", label: "전시/아트 페어" },
+    { id: "musical", label: "연극/뮤지컬" },
+    { id: "concert", label: "콘서트/음악 페스티벌" },
+    { id: "sport", label: "스포츠 경기" },
+    { id: "festival", label: "축제" },
+    { id: "dance", label: "무용/발레" },
+    { id: "classic", label: "클래식/국악 공연" },
+    { id: "cultures", label: "체험형 문화활동" },
+    { id: "etc culture", label: "기타 문화" },
 
-    { category: "basic", id: "etc category", label: "기타 카테고리", width: 116 },
+    { id: "etc", label: "기타 카테고리" },
   ];
 
-  const getButtonStyle = (isSelected) => ({
-    backgroundColor: isSelected ? "#E7C9FF" : "#F0F0F0",
-    height: 38,
-    paddingVertical: 8,
-    borderRadius: 50,
-    borderWidth: 1,
-    borderColor: "#000000",
-    alignItems: "center",
-  });
 
-  const renderCategoryRow = (rowItems) => (
-    <View style={{ flexDirection: 'row', marginBottom: 12 }}>
-      {rowItems.map((item) => {
-        const isSelected = selectedCategory === item.id;
-        return (
-          <View key={item.id} style={{ paddingRight: 12, width: item.width }}>
-            <TouchableOpacity
-              onPress={
-                () => {setSelectedCategory(item.id)
-                console.log("선택된 카테고리:", item.category);
-              }}
-              style={getButtonStyle(isSelected)}> 
-              <Text style={{fontWeight:'bold'}}>{item.label}</Text>
-            </TouchableOpacity>
-          </View>
-        );
-      })}
-    </View>
-  );
+  const contentRows = [
+    categories.slice(0, 3),
+    categories.slice(3, 6),
+    categories.slice(6, 8)
+  ];
+
+  const cultureRows = [
+    categories.slice(8, 10),
+    categories.slice(10, 12),
+    categories.slice(12, 15),
+    categories.slice(15, 17)
+  ];
+
+  const otherRows = [categories.slice(17, 18)];
+
+  const styles = StyleSheet.create({
+    button: {
+      backgroundColor: selectedCategory ? '#C881FF' : '#D9D9D9',
+      width: 343,
+      height: 52,
+      borderRadius: 60,
+      borderColor: selectedCategory ? 'black' : 'gray',
+      borderWidth: 1.5,
+      marginBottom: 20,
+      alignSelf: 'center'
+    },
+    buttontitle: {
+      fontSize: 16,
+      fontWeight: 'bold',
+      color: selectedCategory ? '#FFFFFF' : 'gray',
+      alignSelf: 'center',
+      paddingTop: 11 
+    }
+  })
 
   return (
     <Layout>
-      <View>
-        <View style={{paddingLeft: 18, paddingTop: 28, flexDirection: "row"}} >
-          <TouchableOpacity onPress={() => navigation.goBack()}>
-            <Ionic name="arrow-back"
-              style={{ fontSize:32, color: '#E7C9FF'}} />
-          </TouchableOpacity>
-          <Text style={{paddingLeft: 90, fontSize: 20, fontWeight: "bold"}}>
-            카테고리 설정
-          </Text>
-        </View>
-        
-        <View style={{paddingTop: 46}}>
-          <View style={{width:136, height: 19, top: 46, left: 16, backgroundColor: '#FFE5E2', position: 'absolute'}}></View>
-          <Text style={{fontSize: 18, fontWeight: "bold", position: 'absolute', top: 36, left: 22}}>콘텐츠 카테고리</Text>
-        </View>
 
-        {/* 카테고리 버튼들 */}
-        <View style={{ paddingLeft: 14, paddingTop: 26 }}>
-          {renderCategoryRow(categories.slice(0, 3))}
-          {renderCategoryRow(categories.slice(3, 6))}
-          {renderCategoryRow(categories.slice(6, 8))}
-        </View>
+      <CustomHeader title="카테고리 설정" />
 
-        <View>
-          <View style={{width:121, height: 19, top: 26, left: 16, backgroundColor: '#FFE5E2', position: 'absolute'}}></View>
-          <Text style={{fontSize: 18, fontWeight: "bold", position: 'absolute', top: 16, left: 22}}>문화 카테고리</Text>
-        </View>
+      <View style={{ height: 16 }} />
 
-        <View style={{paddingLeft: 14, paddingTop: 56}}>
-          {renderCategoryRow(categories.slice(8, 10))}
-          {renderCategoryRow(categories.slice(10, 12))}
-          {renderCategoryRow(categories.slice(12, 15))}
-          {renderCategoryRow(categories.slice(15, 17))}
-        </View>
+      <CategoryCard
+        CategoryTitle="콘텐츠 카테고리"
+        categories={contentRows}
+        selectedCategory={selectedCategory}
+        setSelectedCategory={setSelectedCategory}
+      />
 
-        <View>
-          <View style={{width:211, height: 19, top: 26,left: 16, backgroundColor: '#FFE5E2', position: 'absolute'}}></View>
-          <Text style={{fontSize: 18, fontWeight: "bold", position: 'absolute', top: 16, left: 22}}>적절한 카테고리가 없나요?</Text>
-        </View>
+      <CategoryCard
+        CategoryTitle="문화 카테고리"
+        categories={cultureRows}
+        selectedCategory={selectedCategory}
+        setSelectedCategory={setSelectedCategory}
+      />
 
-        <View style={{paddingLeft: 14, paddingTop: 56}}>
-          {renderCategoryRow(categories.slice(17, 18))}
-        </View>
+      <CategoryCard
+        CategoryTitle="적절한 카테고리가 없나요?"
+        categories={otherRows}
+        selectedCategory={selectedCategory}
+        setSelectedCategory={setSelectedCategory}
+      />
 
-        <Button 
-          title="선택 완료"
-          color="#C881FF"
-        />
+      <View style={{ paddingTop: 18 }}>
+        <TouchableOpacity style={styles.button}
+          onPress={() => {
+            if (selectedCategory) {
+              navigation.navigate("Detail", { screen: "Write" , params: { selectedCategoryId: selectedCategory}});
+            }
+          }}
+          disabled={!selectedCategory}
+        >
+
+          <Text style={styles.buttontitle}>선택 완료</Text>
+        </TouchableOpacity>
       </View>
+
     </Layout>
   );
 };
