58b5da6f2f5c7b6ceba207cb21bacc9baaea10e5
강승현 <number42isuser@gmail.com>
2025-05-07 23:58:09 +0900

    ♻️ refactor: 타이포그래피 시스템 모듈화

--- components/login/LoginSection.js
+++ components/login/LoginSection.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Text, View, StyleSheet, TouchableOpacity, Image } from "react-native";
-import { Typography } from "../../styles/Fonts";
+import { Fonts } from "../../styles/Fonts";
 
 const LoginSection = ({ navigation, height }) => {
   return (
     <View style={[styles.container, { height: height }]}>
       <View style={styles.overlineTextContainer}>
-        <Text style={styles.h1}>MoodStudio</Text>
-        <Text style={styles.h2}>다양한 취미를 캐비넷에 모아보세요</Text>
+        <Text style={Fonts.h1}>MoodStudio</Text>
+        <Text style={Fonts.subtitle2}>다양한 취미를 캐비넷에 모아보세요</Text>
       </View>
 
       <TouchableOpacity
@@ -15,7 +15,7 @@ const LoginSection = ({ navigation, height }) => {
         onPress={() => navigation.navigate("Main")}
       >
         {/* <Image source={require("../../assets/icons/login-kakaotalk.svg")} style={styles.image} /> */}
-        <Text style={styles.h2}>카카오톡으로 로그인하기</Text>
+        <Text style={Fonts.subtitle2}>카카오톡으로 로그인하기</Text>
       </TouchableOpacity>
     </View>
   );
@@ -53,16 +53,5 @@ const styles = StyleSheet.create({
   image: {
     width: 24,
     height: 22,
-  },
-  // 텍스트 임시(수정예정)
-  h1: {
-    fontFamily: Typography.Tenada,
-    fontSize: 36,
-    color: "#333",
-  },
-  h2: {
-    fontFamily: Typography.NanumSquareNeo.extraBold,
-    fontSize: 14,
-    color: "#333",
   },
 });
