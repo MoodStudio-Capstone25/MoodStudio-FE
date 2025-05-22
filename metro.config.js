b90ad6c801458abf077ecdc9b78a7e612129a2e3
minseochoo <cloudiao3o@gmail.com>
2025-05-23 03:55:58 +0900

    ➕ deps: 에셋, svg 관련 라이브러리 설치

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,16 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require("expo/metro-config");
 
-/** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
 config.resolver.assetExts.push("glb", "gltf");
+config.resolver.assetExts = config.resolver.assetExts.filter(
+  (ext) => ext !== "svg"
+);
+config.resolver.sourceExts.push("svg");
+
+config.transformer = {
+  ...config.transformer,
+  babelTransformerPath: require.resolve("react-native-svg-transformer"),
+};
 
 module.exports = config;
