7a58c67841f8ac5036037d9d676d6c178e7b34c8
seunghyeonKang <number42isuser@gmail.com>
2025-05-25 00:35:08 +0900

    🎨 design: 캐러셀 인디케이터 닷 UI 추가

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, useWindowDimensions } from "react-native";
+import { StyleSheet, useWindowDimensions, View } from "react-native";
 import Layout from "../layouts/Layout";
 import ScreenCarousel from "../components/login/ScreenCarousel";
 import LoginSection from "../components/login/LoginSection";
@@ -10,6 +10,12 @@ const LoginScreen = ({ navigation }) => {
   return (
     <Layout fullBackgroundCSS={styles.fullBackgroundCSS}>
       <ScreenCarousel height={height * 0.6} />
+      {/* 컴포넌트 기능 추가 예정 */}
+      <View style={styles.indicatorDotWrapper}>
+        <View style={styles.indicatorDotSelected} />
+        <View style={styles.indicatorDot} />
+        <View style={styles.indicatorDot} />
+      </View>
       <LoginSection navigation={navigation} height={height * 0.4} />
     </Layout>
   );
@@ -21,5 +27,31 @@ export default LoginScreen;
 const styles = StyleSheet.create({
   fullBackgroundCSS: {
     backgroundColor: "#FFF4FC",
+  },
+  indicatorDotWrapper: {
+    marginTop: -20,
+    marginBottom: 12,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  indicatorDotSelected: {
+    marginHorizontal: 4,
+    width: 8,
+    height: 8,
+    backgroundColor: "#000",
+    borderRadius: 4,
+    borderWidth: 1,
+    borderColor: "#000",
+  },
+  indicatorDot: {
+    marginHorizontal: 4,
+    width: 6,
+    height: 6,
+    backgroundColor: "#FFF",
+    borderRadius: 3,
+    borderWidth: 1,
+    borderColor: "#000",
   },
 });
