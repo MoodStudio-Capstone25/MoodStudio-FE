e6d53498fe6feecd992cbb8c5cbed3c903f19674
seunghyeonKang <number42isuser@gmail.com>
2025-05-24 23:17:52 +0900

    ♻️ refactor: 파일 분리 및 코드 리팩토링

--- components/list/ReviewList.js
+++ components/list/ReviewList.js
@@ -1,58 +1,46 @@
 import { Image, StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { Fonts } from "../../styles/Fonts";
-
-const ReviewCard = ({ reviewTitle, reviewContents, reviewImage = null }) => {
-  return (
-    <View style={styles.cardContainer}>
-      <View style={styles.cardTextWrapper}>
-        {reviewTitle && (
-          <Text style={[Fonts.subtitle2, styles.cardTitle]} numberOfLines={1}>
-            {reviewTitle}
-          </Text>
-        )}
-        {reviewContents && (
-          <Text style={[styles.cardContents, Fonts.body3]} numberOfLines={reviewTitle ? 2 : 3}>
-            {reviewContents}
-          </Text>
-        )}
-      </View>
-
-      {reviewImage && (
-        <View style={[styles.cardImageWrapper, styles.cardImage]}>
-          <Image
-            style={styles.cardImage}
-            source={require("../../assets/images/login/main-page.png")}
-          />
-        </View>
-      )}
-    </View>
-  );
-};
+import ReviewCard from "./ReviewCard";
 
 const ReviewList = () => {
+  const [reviewDataList, setReviewDataList] = useState([
+    {
+      id: 0,
+      reviewImage: true,
+      reviewTitle: "제목입니다.",
+      reviewContents:
+        "내용입니다내용입니다내용입니다내용입니다. 내용입니다내용입니다.내용입니다내용입니다.",
+    },
+    {
+      id: 1,
+      reviewImage: null,
+      reviewTitle: "현식을 비추는 거울",
+      reviewContents:
+        "우리의 삶을 사실적으로 담아내어 공감을 자아내고 깊은 생각을 하게 만든다. 우리의 삶을 사실적으로 담아내 어 공 감을 자아내고 깊은 생각을 하게 만든다.",
+    },
+    {
+      id: 2,
+      reviewImage: true,
+      reviewTitle: "",
+      reviewContents:
+        "예측할 수 없는 전개와 충격적인 결말이 긴장감을 극대화했다. 예측할 수 없는 전개와 충격적인 결말이 긴장감을 극대화했다. 예측할 수 없는 전개와 충격적인 결말이 긴장감을 극대화했다.",
+    },
+    {
+      id: 3,
+      reviewImage: true,
+      reviewTitle: "",
+      reviewContents:
+        "내용입니다내용입니다내용입니다내용입니다. 내용입니다내용입니다.내용입니다내용입니다.",
+    },
+  ]);
+  // 변수값 초기 세팅 수정 예정(api 연결할 때), 이미지 데이터 형식 수정 예정
+
   return (
-    <View style={styles.listContainer}>
-      {/* 반복함수로 수정 예정, 이미지 코드 수정 예정 */}
-      <ReviewCard
-        reviewTitle="제목입니다."
-        reviewContents="내용입니다내용입니다내용입니다내용입니다. 내용입니다내용입니다.내용입니다내용입니다."
-        reviewImage={true}
-      />
-      <ReviewCard
-        reviewTitle="현식을 비추는 거울"
-        reviewContents="우리의 삶을 사실적으로 담아내어 공감을 자아내고 깊은 생각을 하게 만든다. 우리의 삶을 사실적으로 담아내 어 공 감을 자아내고 깊은 생각을 하게 만든다."
-        reviewImage={null}
-      />
-      <ReviewCard
-        reviewTitle=""
-        reviewContents="예측할 수 없는 전개와 충격적인 결말이 긴장감을 극대화했다. 예측할 수 없는 전개와 충격적인 결말이 긴장감을 극대화했다. 예측할 수 없는 전개와 충격적인 결말이 긴장감을 극대화했다."
-        reviewImage={true}
-      />
-      <ReviewCard
-        reviewContents="내용입니다내용입니다내용입니다내용입니다. 내용입니다내용입니다.내용입니다내용입니다."
-        reviewImage={true}
-      />
+    <View style={styles.container}>
+      {reviewDataList.map((reviewData) => (
+        <ReviewCard key={reviewData.id} {...reviewData} />
+      ))}
     </View>
   );
 };
@@ -60,44 +48,8 @@ const ReviewList = () => {
 export default ReviewList;
 
 const styles = StyleSheet.create({
-  listContainer: {
+  container: {
+    marginTop: 16,
     marginHorizontal: 24,
-  },
-  cardContainer: {
-    marginBottom: 12,
-    padding: 16,
-
-    display: "flex",
-    flexDirection: "row",
-    alignSelf: "stretch",
-
-    alignItems: "center",
-
-    borderRadius: 20,
-    borderWidth: 1,
-    borderColor: "#000",
-    backgroundColor: "#FFF",
-  },
-  cardTextWrapper: {
-    display: "flex",
-    flexGrow: 1,
-    flexShrink: 0,
-    flexBasis: 0,
-  },
-  cardTitle: {
-    marginBottom: 6,
-    textAlign: "justify",
-  },
-  cardContents: {
-    textAlign: "justify",
-  },
-  cardImageWrapper: {
-    marginLeft: 12,
-    overflow: "hidden",
-  },
-  cardImage: {
-    width: 56,
-    height: 56,
-    borderRadius: 8,
   },
 });
