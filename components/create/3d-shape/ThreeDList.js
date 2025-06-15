7c78fdcaa7185fcef9482781f67aeb601af8cacf
seunghyeonKang <number42isuser@gmail.com>
2025-06-16 03:23:14 +0900

    ✨ feat: 이미지 적용

--- components/create/3d-shape/ThreeDList.js
+++ components/create/3d-shape/ThreeDList.js
@@ -1,13 +1,22 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
 import SectionTitle from "../common/SectionTitle";
 import { ScrollView } from "react-native-gesture-handler";
 import { Fonts } from "../../../styles/Fonts";
 
-const ThreeDItem = ({ itemName }) => {
+const threeDImages = {
+  // 추가 예정
+  book: require("../../../assets/images/3d-items/book1.png"),
+  dvd: require("../../../assets/images/3d-items/music1.png"),
+  game: require("../../../assets/images/3d-items/game1.png"),
+};
+
+const ThreeDItem = ({ itemName, itemImage }) => {
   return (
     <TouchableOpacity style={{ marginRight: 12 }}>
-      <View style={styles.imageContainer}></View>
+      <View style={styles.imageContainer}>
+        <Image style={styles.image} source={itemImage} />
+      </View>
       <Text style={[Fonts.subtitle2, styles.itemText]}>{itemName}</Text>
     </TouchableOpacity>
   );
@@ -22,9 +31,12 @@ const ThreeDList = () => {
       contentContainerStyle={{ overflow: "visible" }}
       overScrollMode="never"
     >
-      <ThreeDItem itemName="책" />
-      <ThreeDItem itemName="DVD" />
-      <ThreeDItem itemName="게임" />
+      <ThreeDItem itemName="책" itemImage={threeDImages.book} />
+      <ThreeDItem itemName="DVD" itemImage={threeDImages.dvd} />
+      <ThreeDItem itemName="게임" itemImage={threeDImages.game} />
+      <ThreeDItem itemName="책" itemImage={threeDImages.book} />
+      <ThreeDItem itemName="DVD" itemImage={threeDImages.dvd} />
+      <ThreeDItem itemName="게임" itemImage={threeDImages.game} />
     </ScrollView>
   );
 };
@@ -40,9 +52,13 @@ const styles = StyleSheet.create({
   imageContainer: {
     width: 120,
     height: 120,
-
     borderRadius: 60,
     borderWidth: 1.5,
+  },
+  image: {
+    width: 120,
+    height: 120,
+    borderRadius: 60,
   },
   itemText: {
     textAlign: "center",
