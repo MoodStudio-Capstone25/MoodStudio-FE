acc9071b8d26b26ced513ff7e19f6b0b9378edc3
seunghyeonKang <number42isuser@gmail.com>
2025-06-22 16:22:59 +0900

    📚 docs: 목록 화면 - 이미지 링크 추가 및 카테고리 잘림 현상 수정

--- mock/listDummy.js
+++ mock/listDummy.js
@@ -2,7 +2,7 @@ export const listDummy = [
   // 이미지 데이터 형식 수정 예정
   {
     id: 0,
-    reviewImage: true,
+    reviewImage: "https://picsum.photos/id/0/80/80",
     reviewTitle: "제목입니다.",
     reviewContents:
       "내용입니다내용입니다내용입니다내용입니다. 내용입니다내용입니다.내용입니다내용입니다.",
@@ -20,7 +20,7 @@ export const listDummy = [
   },
   {
     id: 2,
-    reviewImage: true,
+    reviewImage: "https://picsum.photos/id/22/80/80",
     reviewTitle: "",
     reviewContents:
       "예측할 수 없는 전개와 충격적인 결말이 긴장감을 극대화했다. 예측할 수 없는 전개와 충격적인 결말이 긴장감을 극대화했다. 예측할 수 없는 전개와 충격적인 결말이 긴장감을 극대화했다.",
@@ -29,7 +29,7 @@ export const listDummy = [
   },
   {
     id: 3,
-    reviewImage: true,
+    reviewImage: "https://picsum.photos/id/33/80/80",
     reviewTitle: "",
     reviewContents:
       "예측할 수 없는 전개와 충격적인 결말이 긴장감을 극대화했다. 예측할 수 없는 전개와 충격적인 결말이 긴장감을 극대화했다. 예측할 수 없는 전개와 충격적인 결말이 긴장감을 극대화했다.",
@@ -38,7 +38,7 @@ export const listDummy = [
   },
   {
     id: 4,
-    reviewImage: true,
+    reviewImage: "https://picsum.photos/id/44/80/80",
     reviewTitle: "",
     reviewContents:
       "내용입니다내용입니다내용입니다내용입니다. 내용입니다내용입니다.내용입니다내용입니다.",
@@ -65,7 +65,7 @@ export const listDummy = [
   },
   {
     id: 7,
-    reviewImage: null,
+    reviewImage: "https://picsum.photos/id/77/80/80",
     reviewTitle: "웰메이드 서스펜스",
     reviewContents:
       "우리의 삶을 사실적으로 담아내어 공감을 자아내고 깊은 생각을 하게 만든다. 우리의 삶을 사실적으로 담아내 어 공 감을 자아내고 깊은 생각을 하게 만든다.",
