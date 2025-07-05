bce9aac3238c9001eeaf5c523bb28d32a2361fb3
KaHeyon <diddl2002@naver.com>
2025-07-05 17:17:17 +0900

    📚 docs: 상세화면 목데이터 추가

---
+++ mock/detailDummy.js
@@ -0,0 +1,76 @@
+export const detailDummy = [
+    {
+        "id": "1",
+        "template": "content",
+        "category": "책",
+        "data": {
+            "image_urls": [
+                "https://picsum.photos/id/80/800/600",
+                "https://picsum.photos/id/77/800/600",
+            ],
+            "title": "올해 최고의 소설",
+            "api_thumnail": null, //책,영화 api에서 가져오는 썸네일
+            "rating": 4.8,
+            "date": "2025-04-15",
+            "content_title": "에브리씽 에브리웨어 올 앳 원스",
+            "creator": ["클레이키컨"],
+            "cast": ["출연진1", "출연진2", "출연진3", "출연진4", "출연진5"],
+            "story": "멀티버스를 배경으로 한 이야기...",
+            "scenes": "모든 우주에서의 엄마의 사랑...",
+            "thoughts": "삶의 의미를 다시 생각하게 만든 작품",
+            "location": null,
+            "companions": null,
+            "created_at": "2025-04-19",
+            "updated_at": "2025-04-19"
+        }
+    },
+    {
+        "id": "2",
+        "template": "basic",
+        "category": "책",
+        "data": {
+            "image_urls": [
+                "https://picsum.photos/id/119/800/600",
+                "https://picsum.photos/id/16/800/600",
+                "https://picsum.photos/id/25/800/600",
+            ],
+            "title": "올해 최고의 소설",
+            "api_thumnail": null,
+            "rating": null,
+            "date": "2025.04.02",
+            "content_title": null,
+            "creator": null,
+            "cast": null,
+            "story": null,
+            "scenes": null,
+            "thoughts": "삶의 의미를 다시 생각하게 만든 작품",
+            "location": null,
+            "companions": null,
+            "created_at": "2025-04-19",
+            "updated_at": "2025-04-19"
+        }
+    },
+    {
+        "id": "3",
+        "template": "culture",
+        "category": "책",
+        "data": {
+            "image_urls": null,
+            "title": "올해 최고의 소설",
+            "api_thumnail": null, //책,영화 api에서 가져오는 썸네일
+            "rating": null,
+            "date": "2025-04-15 ~ 2025-04-16",
+            "content_title": "에브리씽 에브리웨어 올 앳 원스",
+            "creator": ["클레이키컨"],
+            "cast": ["출연진1", "출연진2", "출연진3", "출연진4", "출연진5"],
+            "story": "멀티버스를 배경으로 한 이야기...",
+            "scenes": "모든 우주에서의 엄마의 사랑...",
+            "thoughts": "삶의 의미를 다시 생각하게 만든 작품",
+            "location": "공연장",
+            "companions": "엄마",
+            "created_at": "2025-04-19",
+            "updated_at": "2025-04-19"
+        }
+    },
+
+]
