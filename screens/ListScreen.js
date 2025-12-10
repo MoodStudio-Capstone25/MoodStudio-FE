1012330a8aa84d86d6a688b419fb0f18540bcb2c
seunghyeonKang <number42isuser@gmail.com>
2025-12-11 06:36:02 +0900

    ✨ feat: 목록 페이지 api 연결

--- screens/ListScreen.js
+++ screens/ListScreen.js
@@ -1,5 +1,9 @@
+import axios from "axios";
+import { BACKEND_API_URL } from "@env";
+import * as SecureStore from "expo-secure-store";
+
 import { Animated, Text, View } from "react-native";
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Layout from "../layouts/Layout";
 
 import { ListHeader } from "../components/common/topbar/Header";
@@ -8,7 +12,27 @@ import ReviewList from "../components/list/ReviewList";
 import SortSelector from "../components/list/SortSelector";
 import { listDummy } from "../mock/listDummy";
 
+const fetchRecords = async (token) => {
+  try {
+    const response = await axios.get(`${BACKEND_API_URL}/records/`, {
+      headers: {
+        Authorization: `Bearer ${token}`, // Bearer 토큰
+      },
+    });
+
+    // 응답 예시: [{ id, template, category, user, image_urls, title, api_thumbnail, rating, date, content_title, creator, cast, story, scenes, thoughts, location, companions, created_at, updated_at }, ...]
+    return response.data;
+  } catch (error) {
+    console.log("GET /records/ 에러:", error.response?.data || error.message);
+    throw error;
+  }
+};
+
 const ListScreen = () => {
+  const [records, setRecords] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
   // 수정날짜/만든날짜 기능 추가 필요
   const [sortBy, setSortBy] = useState("modifiedAt");
   const [sortDirection, setSortDirection] = useState("desc");
@@ -37,6 +61,36 @@ const ListScreen = () => {
 
     setLastScrollY(currentY);
   };
+
+  useEffect(() => {
+    const loadRecords = async () => {
+      try {
+        setLoading(true);
+        setError(null);
+
+        // 토큰 가져오기
+        const token = await SecureStore.getItemAsync("access");
+
+        if (!token) {
+          setError("로그인이 필요합니다.");
+          setLoading(false);
+          return;
+        }
+
+        const data = await fetchRecords(token);
+
+        console.log("data >>>", data);
+
+        setRecords(data);
+      } catch (err) {
+        setError("목록을 불러오는 중 오류가 발생했습니다.");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    loadRecords();
+  }, []);
 
   return (
     <Layout>
